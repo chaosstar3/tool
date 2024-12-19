a78a0865018eb9a64f449e086b1a668a16e98762
song@mm <chaosstar3@gmail.com>
2024-12-20 08:35:09 +0900

    json lines

--- json/json.rb
+++ json/json.rb
@@ -10,9 +10,29 @@ OptionParser.new do |ops|
 	ops.on("-c", "--compress", "compress") {opts[:mode] = :c}
 	ops.on("-p", "--pretty", "pretty") {opts[:mode] = :p}
 	ops.on("-d", "--debug", "debug") {opts[:mode] = :d}
+	ops.on("-l", "--lines", "lines") {opts[:lines] = true}
 end.parse!
 
-json = JSON.parse(ARGF.read)
+# detect_lines
+first_line = ""
+unless opts[:lines]
+	first_line = ARGF.readline
+	begin
+		json = [JSON.parse(first_line)]
+		opts[:lines] = true
+	rescue JSON::ParserError
+		opts[:lines] = false
+	end
+end
+
+# read json
+if opts[:lines]
+	ARGF.readlines.each do |line|
+		json.append JSON.parse(line)
+	end
+else
+	json = JSON.parse(first_line + ARGF.read)
+end
 
 case opts[:mode]
 when :c
